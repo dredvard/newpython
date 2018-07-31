3625fc2708a3f9332fe400d7f1025bf9e6db2417
dredvard <ekambulow@gmail.com>
2018-07-31 13:12:53 -0400

    Add files via upload

--- 3d plot.py
+++ 3d plot.py
@@ -15,17 +15,33 @@ def create_arc(center_x,center_y,radius,theta0,theta,height_z):
     y_grid = radius*np.sin(theta_grid) + center_y
     return x_grid,y_grid,z_grid
 
-def create_wall(center_x,center_y,radius,theta0,theta,height_z):
-    xx, yy = np.meshgrid(range(10), range(10))
+
+def pol2cart(radius,theta, units='deg'):
+    if units=='deg':
+        u=2*np.pi/360
+    else:
+        u=1
+    x=radius*np.cos(u*theta)
+    y=radius*np.sin(u*theta)
+    return x,y
+
+def create_wall(center_x,center_y,radius,theta,height_z):
+
     z = np.linspace(0, height_z, 2)
+    r = np.linspace(0, radius, 10)
+    r_grid, z_grid=np.meshgrid(r, z)
+#    x_grid,y_grid = pol2cart(r_grid,theta+theta0) + (center_x,center_y)
+    x_grid,y_grid = pol2cart(r_grid,theta)
+    return x_grid+center_x,y_grid+center_y,z_grid
+
+def create_lid(center_x,center_y,radius,theta0,theta,height_z):
+    r = np.linspace(0, radius, 10)
     theta = np.linspace(np.pi*2*theta0/360, np.pi*2*theta/360, 25)
-    theta_grid, z_grid=np.meshgrid(theta, z)
-    wall=[(radius,theta,z),(radius,theta,0),(0,theta0,0),(0,theta,z)]
-    wall0=[(radius,theta0,z),(radius,theta0,0),(0,theta0,0),(0,theta0,z)]
-    x_grid = radius*np.cos(theta_grid) + center_x
-    y_grid = radius*np.sin(theta_grid) + center_y
+    theta_grid, r_grid,z_grid=np.meshgrid(theta, r, height_z)
+    x_grid,y_grid = pol2cart(r_grid,theta_grid)
     return x_grid,y_grid,z_grid
 
+from matplotlib.colors import LightSource
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 fig = plt.figure()
@@ -38,11 +54,37 @@ ax = fig.add_subplot(111, projection='3d')
 
 def draw_slice(theta0,theta,r,h,colors='b'):
     #cntr={x:0,y:0}
+    ls = LightSource(azdeg=0, altdeg=65)
+    # Shade data, creating an rgb array.
     Xc,Yc,Zc = create_arc(0,0,r,theta0,theta,h)
-    ax.plot_surface(Xc, Yc, Zc, color=colors,linewidth=10, edgecolor='b')
-
-draw_slice(0,90,1,1)
-draw_slice(-90,0,1,1,'r')
-draw_slice(90,270,1,1,'g')
+    rgb = ls.shade(Zc, plt.cm.RdYlBu)
+    ax.plot_surface(Xc, Yc, Zc, 
+                    rstride=1, cstride=1, 
+                    antialiased=False,
+                    color=colors,linewidth=1, edgecolor='b',facecolors=rgb)
     
+    Xc,Yc,Zc = create_wall(0,0,r,theta0,h)
+    rgb = ls.shade(Zc, plt.cm.RdYlBu)
+    ax.plot_surface(Xc, Yc, Zc, 
+                    rstride=1, cstride=1, 
+                    antialiased=False,
+                    color=colors,linewidth=1, edgecolor='b',facecolors=rgb)
+    Xc,Yc,Zc = create_wall(0,0,r,theta0+theta,h)
+    rgb = ls.shade(Zc, plt.cm.RdYlBu)
+    ax.plot_surface(Xc, Yc, Zc, 
+                    rstride=1, cstride=1, 
+                    antialiased=False,
+                    color=colors,linewidth=1, edgecolor='b',facecolors=rgb)
+    Xc,Yc,Zc = create_lid(0,0,r,theta0,theta,h)
+    #rgb = ls.shade(Xc, plt.cm.RdYlBu)
+    ax.plot_surface(Xc, Yc, Zc, 
+                    rstride=1, cstride=1, 
+                    antialiased=False,
+                    color=colors,linewidth=1, edgecolor='b',facecolors=rgb)
+    
+draw_slice(0,90,1,1)
+#draw_slice(-90,0,1,1,'r')
+#draw_slice(90,270,1,1,'g')
+
+ax.set_axis_off()    
 plt.show()
